56b62e03a299708549394ad24f9ee5b142489e11
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-25 11:35:22 +0600

    donation page added

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import DonationCard from "./DonationCard";
 
 
 const Donation = () => {
+    const [donation, setDonation] = useState()
+    const [noFound, setNoFound] = useState(false)
+
+    useEffect(() => {
+        const donationItems = JSON.parse(localStorage.getItem('donation'))
+        if (donationItems) {
+            setDonation(donationItems)
+        }
+        else {
+            setNoFound('no data found');
+        }
+    }, [])
+
     return (
         <div>
-            <h1>This is donation page</h1>
+            {noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> :
+                <div>
+                    <div className="grid grid-cols-2">
+                        {
+                            donation.map(donate => <DonationCard key={donate.id} donate={donate}></DonationCard>)
+                        }
+                    </div>
+                </div>
+                }
         </div>
     );
 };
