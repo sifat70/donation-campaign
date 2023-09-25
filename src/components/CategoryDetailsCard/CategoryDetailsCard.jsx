c2b1fb6ffa62af6c1fec8a1b91eba318655196ee
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-25 09:30:56 +0600

    added aweet alert and some change of category

--- src/components/CategoryDetailsCard/CategoryDetailsCard.jsx
+++ src/components/CategoryDetailsCard/CategoryDetailsCard.jsx
@@ -1,10 +1,40 @@
+import swal from "sweetalert";
 
 
 const CategoryDetailsCard = ({ category }) => {
-    const { id, image, title_name, category_name, description } = category
+    const { id, image, title_name, description } = category;
+
+
+    const handleAddToDonation = () => {
+        const addedDonationArray = [];
+
+        const donationItems = JSON.parse(localStorage.getItem('donation'))
+
+        if (!donationItems) {
+            addedDonationArray.push(category)
+            localStorage.setItem('donation', JSON.stringify(addedDonationArray))
+            swal("Good job!", "category added", "success");
+        }
+
+        else {
+            const isExits = donationItems.find(category => category.id === id)
+
+            if (!isExits) {
+                addedDonationArray.push(...donationItems, category)
+                localStorage.setItem('donation', JSON.stringify(addedDonationArray))
+                swal("Good job!", "category added", "success");
+            }
+            else{
+                swal("error!", "already added", "error");
+            }
+
+
+        }
+    }
     return (
         <div className="mt-10">
             <img className="w-[100%] h-[70vh] mx-auto" src={image} alt="" />
+            <button onClick={handleAddToDonation} className="btn absolute top-3/4 left-36">Donate $290</button>
             <h1 className="text-3xl font-bold  mt-5">{title_name}</h1>
             <p className=" mt-5 mb-7">{description}</p>
         </div>
